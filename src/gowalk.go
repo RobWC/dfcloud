4b3f3fa03a6bbc35142982c86eeeb7261f87dc3c
Rob Cameron <rwcameron@gmail.com>
2014-03-02 09:35:50 -0800

    finishing cleanup

--- src/gowalk.go
+++ src/gowalk.go
@@ -5,23 +5,40 @@ import (
     "os"
     "dffiles"
     "fmt"
-    "encoding/json"
+   // "encoding/json"
+    "runtime"
+    "sync"
 )
 
 var fileList []dffiles.File
+var wg sync.WaitGroup
+
+func testFile(f *dffiles.File) {
+    defer wg.Done()
+    f.HashValue = f.CheckFile()
+}
+
+func printFinal() {
+    fmt.Printf("Total File Count: %d\n",len(fileList))
+//    b, _ := json.Marshal(fileList)
+  //  os.Stdout.Write(b)
+}
 
 func main(){
+  runtime.GOMAXPROCS(runtime.NumCPU())
   filepath.Walk("./save", func(path string, info os.FileInfo, err error) error {
-    var testFile dffiles.File
+    var newFile dffiles.File
     if (!info.IsDir()) {
-      testFile.Name = info.Name()
-      testFile.Path = path
-      testFile.HashValue = testFile.CheckFile()
-      fileList = append(fileList,testFile)
+      newFile.Name = info.Name()
+      newFile.Path = path
+      fileList = append(fileList,newFile)
     }
     return nil
   })
-  fmt.Printf("Total File Count: %d\n",len(fileList))
-  b, _ := json.Marshal(fileList)
-  os.Stdout.Write(b)
+  for key := range fileList {
+      wg.Add(1)
+      go testFile(&fileList[key])
+  }
+  wg.Wait()
+  printFinal()
 }
